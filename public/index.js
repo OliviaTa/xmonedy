2a07801b964203e070bcde53aa0fcbc47caa29bb
OliviaTa <ir.klypina@gmail.com>
2021-02-19 18:46:39 +0300

    small changes after review

--- public/index.js
+++ public/index.js
@@ -2,9 +2,20 @@
 let menuIcon = document.querySelector('.page-header .menu-icon');
 let mainNav = document.querySelector('.page-header .main-nav');
 let body = document.querySelector('body');
+let mainNavElements = document.querySelectorAll('.page-header .main-nav li')
 
 menuIcon.onclick = function() {
     menuIcon.classList.toggle('change');
     mainNav.classList.toggle('change');
     body.classList.toggle('change');
+}
+
+for (let element of mainNavElements) {
+    element.onclick = function() {
+        if (mainNav.classList.contains('change')) {
+            menuIcon.classList.remove('change');
+            mainNav.classList.remove('change');
+            body.classList.remove('change');
+        }
+    }
 }
