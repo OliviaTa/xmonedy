0a525d38c9fa1ccfbd8899bee254f6b1c676aa77
OliviaTa <ir.klypina@gmail.com>
2021-02-25 14:59:10 +0300

    add yandex metrika

--- public/index.js
+++ public/index.js
@@ -19,3 +19,17 @@ for (let element of mainNavElements) {
         }
     }
 }
+
+function upperButton() {
+    // Yandex.Metrika
+    ym(72874123, 'reachGoal', 'upperButton');
+    return true;
+    // Yandex.Metrika
+}
+
+function lowerButton() {
+    // Yandex.Metrika
+    ym(72874123, 'reachGoal', 'lowerButton');
+    return;
+    // Yandex.Metrika
+}
