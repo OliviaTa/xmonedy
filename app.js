9df3ccf3bd690f4ed0dfd8dc654630106a339639
irina <ir.klypina@gmail.com>
2021-03-12 15:06:30 +0300

    add pug

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
+
+app.set('views', path.join(__dirname, 'public'));
+app.set("view engine", "pug");
+
+app.use("/", function(request, response){
+      
+    response.render("index");
+});
 
 app.listen(3000);
