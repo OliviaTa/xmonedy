6cb985c6a6c9482b14b8389219fe1f20125e3671
irina <ir.klypina@gmail.com>
2021-03-18 15:25:35 +0300

    add smooth scroll

--- app/js/index.js
+++ app/js/index.js
@@ -10,3 +10,19 @@ function lowerButton() {
     return;
 }
 
+// slow scroll
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (const anchor of anchors) {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const blockID = anchor.getAttribute('href').substr(1);
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+}
+
